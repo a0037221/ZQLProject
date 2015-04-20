95bd0ae78ad483271f43a0b86c3e85957c72dd4f
qingliang <a0037221@sohu.com>
2015-04-20 17:00:51 +0800

    Initial Commit

---
+++ 10-瀑布流lianxi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  10-瀑布流lianxi
+//
+//  Created by qingLiang on 15/4/20.
+//  Copyright (c) 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 10-瀑布流lianxi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  10-瀑布流lianxi
+//
+//  Created by qingLiang on 15/4/20.
+//  Copyright (c) 2015年 itcast. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
